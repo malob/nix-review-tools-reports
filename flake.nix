b8e72e308168169718030b869b10ec9959a6368e
Malo Bourgon <mbourgon@gmail.com>
2022-03-16 14:22:43 -0700

    Use writeShellApplication instead of writeScriptBin

--- flake.nix
+++ flake.nix
@@ -22,48 +22,76 @@
       rec {
 
         packages = rec {
-          jobset-latest-successful-eval-id = pkgs.writeShellScriptBin "jobset-latest-successful-eval-id" ''
-            ${curl} -s -L -H 'Accept: application/json' https://hydra.nixos.org/jobset/$1/$2/latest-eval \
-            | ${jq} .id \
-          '';
+          jobset-latest-successful-eval-id = pkgs.writeShellApplication {
+            name = "jobset-latest-successful-eval-id";
+            runtimeInputs = with pkgs; [ curl jq ];
+            text = ''
+              curl -s -L -H 'Accept: application/json' https://hydra.nixos.org/jobset/"$1"/"$2"/latest-eval \
+              | jq .id
+            '';
+          };
 
-          jobset-latest-eval-id = pkgs.writeShellScriptBin "jobset-latest-eval-id" ''
-            ${curl} -s -H 'Accept: application/json' https://hydra.nixos.org/jobset/$1/$2/evals \
-            | ${jq} .evals[0].id \
-          '';
+          jobset-latest-eval-id = pkgs.writeShellApplication {
+            name = "jobset-latest-eval-id";
+            runtimeInputs = with pkgs; [ curl jq ];
+            text = ''
+              curl -s -H 'Accept: application/json' https://hydra.nixos.org/jobset/"$1"/"$2"/evals \
+              | jq .evals[0].id
+            '';
+          };
 
-          jobset-eval-date = pkgs.writeShellScriptBin "jobset-eval-date" ''
-            ${curl} -s -H 'Accept: application/json' https://hydra.nixos.org/eval/$1 \
-            | ${jq} .timestamp \
-            | ${xargs} -I _ ${date} -Idate -d @_ \
-          '';
+          jobset-eval-date = pkgs.writeShellApplication {
+            name = "jobset-eval-date";
+            runtimeInputs = with pkgs; [ coreutils curl findutils jq ];
+            text = ''
+              curl -s -H 'Accept: application/json' https://hydra.nixos.org/eval/"$1" \
+              | jq .timestamp \
+              | xargs -I _ date -Idate -d @_
+            '';
+          };
 
-          gen-report = pkgs.writeShellScriptBin "gen-report" ''
-            mkdir -p data
-            mkdir -p _posts
-            cd data
-            id=$(${jobset-latest-eval-id}/bin/jobset-latest-eval-id $1 $2)
-            successid=$(${jobset-latest-successful-eval-id}/bin/jobset-latest-successful-eval-id $1 $2)
-            date=$(${jobset-eval-date}/bin/jobset-eval-date $id)
-            file=../_posts/$date-$1_$2_$id.md
-            if [ "$id" = "$successid" ]; then
-              echo -e "---\ntitle: $1:$2 $id (succeeded)\ncategories: $1:$2\n---" > $file
-            else
-              echo -e "---\ntitle: $1:$2 $id\ncategories: $1:$2\n---" > $file
-            fi
-            nix-shell ${nix-review-tools}/shell.nix --run "${nix-review-tools}/eval-report $id" >> $file
-            rm eval_$id
-          '';
+          gen-report = pkgs.writeShellApplication {
+            name = "gen-report";
+            runtimeInputs = [
+              jobset-eval-date
+              jobset-latest-eval-id
+              jobset-latest-successful-eval-id
+            ];
+            text = ''
+              mkdir -p data
+              mkdir -p _posts
+              cd data
 
-          rm-reports-older-than = pkgs.writeShellScriptBin "rm-reports-older-than" ''
-            date=$(${date} -Iseconds --date "-$1 $2")
-            git ls-files _posts | while read path
-            do
-              if [ "$(git log --since \"$date\" -- $path)" == "" ]; then
-                rm $path
+              id=$(jobset-latest-eval-id "$1" "$2")
+              successid=$(jobset-latest-successful-eval-id "$1" "$2")
+              date=$(jobset-eval-date "$id")
+              file=../_posts/$date-$1_$2_$id.md
+
+              if [ "$id" = "$successid" ]; then
+                echo -e "---\ntitle: $1:$2 $id (succeeded)\ncategories: $1:$2\n---" > "$file"
+              else
+                echo -e "---\ntitle: $1:$2 $id\ncategories: $1:$2\n---" > "$file"
               fi
-            done
-          '';
+
+              nix-shell ${nix-review-tools}/shell.nix --run \
+                "${nix-review-tools}/eval-report $id" >> "$file"
+              rm "eval_$id"
+            '';
+          };
+
+          rm-reports-older-than = pkgs.writeShellApplication {
+            name = "rm-reports-older-than";
+            runtimeInputs = with pkgs; [ coreutils git ];
+            text = ''
+              date=$(date -Iseconds --date "-$1 $2")
+              git ls-files _posts | while read -r path
+              do
+                if [ "$(git log --since "$date" -- "$path")" == "" ]; then
+                  rm "$path"
+                fi
+              done
+            '';
+          };
         };
         defaultPackage = packages.gen-report;
         apps.gen-report = flake-utils.lib.mkApp { drv = packages.gen-report; };
@@ -77,7 +105,7 @@
             {
               name = "serve";
               command = "bundle exec jekyll serve --incremental";
-              help = "Test Jekyll site locally";
+              help = "test Jekyll site locally";
             }
           ];
         };
