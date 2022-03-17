b997bbfe8c935443be6ce3fafde6ab95ae1ec469
Malo Bourgon <mbourgon@gmail.com>
2022-03-17 14:54:12 -0700

    Don't use `rec` syntax

--- flake.nix
+++ flake.nix
@@ -10,91 +10,97 @@
 
   outputs = { self, nixpkgs, flake-utils, nix-review-tools, devshell }:
     flake-utils.lib.eachDefaultSystem (system:
-
       let
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) writeShellApplication;
         inherit (pkgs.lib) attrValues;
-      in
 
-      rec {
-        packages = rec {
-          jobset-latest-successful-eval-id = writeShellApplication {
-            name = "jobset-latest-successful-eval-id";
-            runtimeInputs = attrValues { inherit (pkgs) curl jq; };
-            text = ''
-              curl -s -L -H 'Accept: application/json' https://hydra.nixos.org/jobset/"$1"/"$2"/latest-eval \
-              | jq .id
-            '';
-          };
-
-          jobset-latest-eval-id = writeShellApplication {
-            name = "jobset-latest-eval-id";
-            runtimeInputs = attrValues { inherit (pkgs) curl jq; };
-            text = ''
-              curl -s -H 'Accept: application/json' https://hydra.nixos.org/jobset/"$1"/"$2"/evals \
-              | jq .evals[0].id
-            '';
-          };
-
-          jobset-eval-date = writeShellApplication {
-            name = "jobset-eval-date";
-            runtimeInputs = attrValues { inherit (pkgs) coreutils curl findutils jq; };
-            text = ''
-              curl -s -H 'Accept: application/json' https://hydra.nixos.org/eval/"$1" \
-              | jq .timestamp \
-              | xargs -I _ date -Idate -d @_
-            '';
-          };
-
-          gen-report = writeShellApplication {
-            name = "gen-report";
-            runtimeInputs = [
-              jobset-eval-date
-              jobset-latest-eval-id
-              jobset-latest-successful-eval-id
-            ];
-            text = ''
-              mkdir -p data
-              mkdir -p _posts
-              cd data
-
-              id=$(jobset-latest-eval-id "$1" "$2")
-              successid=$(jobset-latest-successful-eval-id "$1" "$2")
-              date=$(jobset-eval-date "$id")
-              file=../_posts/$date-$1_$2_$id.md
-
-              if [ "$id" = "$successid" ]; then
-                echo -e "---\ntitle: $1:$2 $id (succeeded)\ncategories: $1:$2\n---" > "$file"
-              else
-                echo -e "---\ntitle: $1:$2 $id\ncategories: $1:$2\n---" > "$file"
-              fi
-
-              nix-shell ${nix-review-tools}/shell.nix --run \
-                "${nix-review-tools}/eval-report $id" >> "$file"
-              rm "eval_$id"
-            '';
-          };
-
-          rm-reports-older-than = writeShellApplication {
-            name = "rm-reports-older-than";
-            runtimeInputs = attrValues { inherit (pkgs) coreutils git; };
-            text = ''
-              date=$(date -Iseconds --date "-$1 $2")
-              git ls-files _posts | while read -r path
-              do
-                if [ "$(git log --since "$date" -- "$path")" == "" ]; then
-                  rm "$path"
-                fi
-              done
-            '';
-          };
-
-          default = gen-report;
+        jobset-latest-successful-eval-id = writeShellApplication {
+          name = "jobset-latest-successful-eval-id";
+          runtimeInputs = attrValues { inherit (pkgs) curl jq; };
+          text = ''
+            curl -s -L -H 'Accept: application/json' https://hydra.nixos.org/jobset/"$1"/"$2"/latest-eval \
+            | jq .id
+          '';
         };
 
+        jobset-latest-eval-id = writeShellApplication {
+          name = "jobset-latest-eval-id";
+          runtimeInputs = attrValues { inherit (pkgs) curl jq; };
+          text = ''
+            curl -s -H 'Accept: application/json' https://hydra.nixos.org/jobset/"$1"/"$2"/evals \
+            | jq .evals[0].id
+          '';
+        };
+
+        jobset-eval-date = writeShellApplication {
+          name = "jobset-eval-date";
+          runtimeInputs = attrValues { inherit (pkgs) coreutils curl findutils jq; };
+          text = ''
+            curl -s -H 'Accept: application/json' https://hydra.nixos.org/eval/"$1" \
+            | jq .timestamp \
+            | xargs -I _ date -Idate -d @_
+          '';
+        };
+
+        gen-report = writeShellApplication {
+          name = "gen-report";
+          runtimeInputs = [
+            jobset-eval-date
+            jobset-latest-eval-id
+            jobset-latest-successful-eval-id
+          ];
+          text = ''
+            mkdir -p data
+            mkdir -p _posts
+            cd data
+
+            id=$(jobset-latest-eval-id "$1" "$2")
+            successid=$(jobset-latest-successful-eval-id "$1" "$2")
+            date=$(jobset-eval-date "$id")
+            file=../_posts/$date-$1_$2_$id.md
+
+            if [ "$id" = "$successid" ]; then
+              echo -e "---\ntitle: $1:$2 $id (succeeded)\ncategories: $1:$2\n---" > "$file"
+            else
+              echo -e "---\ntitle: $1:$2 $id\ncategories: $1:$2\n---" > "$file"
+            fi
+
+            nix-shell ${nix-review-tools}/shell.nix --run \
+              "${nix-review-tools}/eval-report $id" >> "$file"
+            rm "eval_$id"
+          '';
+        };
+
+        rm-reports-older-than = writeShellApplication {
+          name = "rm-reports-older-than";
+          runtimeInputs = attrValues { inherit (pkgs) coreutils git; };
+          text = ''
+            date=$(date -Iseconds --date "-$1 $2")
+            git ls-files _posts | while read -r path
+            do
+              if [ "$(git log --since "$date" -- "$path")" == "" ]; then
+                rm "$path"
+              fi
+            done
+          '';
+        };
+
+        scripts = {
+          inherit
+            jobset-latest-successful-eval-id
+            jobset-latest-eval-id
+            jobset-eval-date
+            gen-report
+            rm-reports-older-than;
+        };
+      in
+
+      {
+        packages = scripts // { default = gen-report; };
+
         devShell = devshell.legacyPackages.${system}.mkShell {
-          packages = [ pkgs.ruby ] ++ attrValues packages;
+          packages = [ pkgs.ruby ] ++ attrValues scripts;
           commands = [
             {
               name = "serve";
@@ -103,7 +109,7 @@
             }
           ];
         };
-
       }
+
     );
 }
