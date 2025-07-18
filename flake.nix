d0f7467a4f31fcb1c02852cadf402a2d5b2b62dd
Malo Bourgon <mbourgon@gmail.com>
2025-07-18 10:54:02 -0700

    Update to fork of `nix-review-tools` with custom user agent

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    nix-review-tools = { url = "github:malob/nix-review-tools/improve-reports"; flake = false; };
+    nix-review-tools = {
+      url = "github:malob/nix-review-tools/nix-review-tools-reports";
+      flake = false;
+    };
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
     devshell.inputs.flake-utils.follows = "flake-utils";
